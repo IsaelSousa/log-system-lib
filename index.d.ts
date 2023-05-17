8bc0a27530e1d9f5d27ed2c55bdd683e1e16a917
IsaelSousa <toxicrootbeta@gmail.com>
2023-05-17 00:00:46 -0300

    feat: new logs types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,18 @@
-export { startLog, LogMessage } from './lib/socket';
+import { startLog } from './lib/socket';
+
+declare function trace(message: string): void
+declare function debug(message: string): void
+declare function info(message: string): void
+declare function warn(message: string): void
+declare function error(message: string): void
+declare function fatal(message: string): void
+
+export = { 
+    startLog,
+    trace,
+    debug,
+    info,
+    warn,
+    error,
+    fatal
+ }
